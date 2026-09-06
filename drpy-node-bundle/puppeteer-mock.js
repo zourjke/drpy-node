7e2229c3dd006a48e34ddde26fa8c0593dc19e34
zourjke <zourjke@outlook.com>
2026-09-06 12:31:34 +0800

    remove: drpy-node-bundle/puppeteer-mock.js

--- drpy-node-bundle/puppeteer-mock.js
+++
@@ -1,3 +0,0 @@
-export const launch = async () => { throw new Error("Puppeteer is not available in the bundled version."); };
-export const executablePath = () => "mock-path";
-export default { launch, executablePath };
