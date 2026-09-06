ffe4088191a3af91864e0c08a820f07df623b2c9
zourjke <zourjke@outlook.com>
2026-09-06 12:31:34 +0800

    remove: drpy-node-bundle/localt5.js

--- drpy-node-bundle/localt5.js
+++
@@ -1,7 +0,0 @@
-// process.env.PATH = `${process.env.PATH}:/data/data/com.github.yingtu/files/plugin/php`;
-// process.env.LD_LIBRARY_PATH = `${process.env.LD_LIBRARY_PATH || ''}:/data/data/com.github.yingtu/files/plugin/php/libs`;
-import * as localtDsCore from './libs/localDsCore.bundled.js';
-
-console.log(getEngine);
-// global.localtDsCore = localtDsCore;
-// global.getEngine = getEngine;
